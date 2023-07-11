d13f987df49772f69f6fc20a6ac9c9d0d1887263
thakkardhavalc <thakkardhavalc@gmail.com>
2023-07-11 12:45:06 -0600

    initial commit

---
+++ src/test/java/guru/springframework/spring6webclient/Spring6WebclientApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring6webclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6WebclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
